e815ee42518a7ae75c087129ed80d96e249acd01
Moisés Alex de Castro Macedo <91707010+moises-macedo@users.noreply.github.com>
2022-08-28 20:03:36 -0300

    Create global.js

---
+++ src/Assets/Styles/global.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+:root{
+
+}
+*{
+margin: 0;
+padding: 0;
+border: 0;
+text-decoration: none;
+list-style: none;
+box-sizing: border-box;
+
+}
+`
